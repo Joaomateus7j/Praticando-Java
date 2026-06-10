4b269d0ed3e759a210cf6ce9385645a75c3f9c57
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 21:15:04 -0300

    Primeiro exercicio

--- src/PrimeiroExercicio.java
+++ src/PrimeiroExercicio.java
@@ -1,2 +1,9 @@
 public class PrimeiroExercicio {
+    public static void main(String[] args) {
+
+        // Testando Repositorio novo !!!
+
+        String teste = "Testando onde vai ser o meus novos exercicios de Java";
+        System.out.println(teste);
+    }
 }
