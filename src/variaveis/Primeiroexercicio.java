a71e642ec531bf2fd974f729e06db75ca0669653
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 21:33:56 -0300

    Teste

---
+++ src/variaveis/Primeiroexercicio.java
@@ -0,0 +1,9 @@
+package variaveis;
+
+public class Primeiroexercicio {
+    public static void main(String[] args) {
+
+        String teste2 = "Estou testando e criando minha pasta de váriavel";
+        System.out.println(teste2);
+    }
+}
