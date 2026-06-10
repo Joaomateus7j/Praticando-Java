81ecfd1d5e2840caa272ce1ae61b5811f4f89567
João Mateus <mjoaomateus10@gmail.com>
2026-06-09 21:54:09 -0300

    Testando a pasta de Condicionais

---
+++ src/condicionais/Exercicio1.java
@@ -0,0 +1,9 @@
+package condicionais;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+
+        String teste2 = "Estou testando e criando minha pasta de váriavel, ";
+        System.out.println(teste2);
+    }
+}
