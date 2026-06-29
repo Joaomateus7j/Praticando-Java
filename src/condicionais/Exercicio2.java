e38159131e352eea20984de4551f87047d1b5e0b
João Mateus <mjoaomateus10@gmail.com>
2026-06-29 19:10:35 -0300

    Praticando Condicionais

---
+++ src/condicionais/Exercicio2.java
@@ -0,0 +1,11 @@
+package condicionais;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+
+        if(true){
+            System.out.println("Dentro do if");
+        }
+        System.out.println("Fora de if");
+    }
+}
