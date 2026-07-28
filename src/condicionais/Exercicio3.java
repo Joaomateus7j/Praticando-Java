7815014e11ddaae21ce3b729be83ee39ed32c9b7
João Mateus <mjoaomateus10@gmail.com>
2026-07-28 10:37:26 -0300

    Aplicando

--- src/condicionais/Exercicio3.java
+++ src/condicionais/Exercicio3.java
@@ -3,5 +3,6 @@ package condicionais;
 public class Exercicio3 {
     public static void main(String[] args) {
         System.out.println("Teste de escrita no Java !");
+        System.out.println("Teste de escrita no Java !");
     }
 }
