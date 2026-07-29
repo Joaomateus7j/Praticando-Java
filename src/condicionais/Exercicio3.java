c06b85555736e6142b844e0027d1342686143685
João Mateus <mjoaomateus10@gmail.com>
2026-07-29 11:54:21 -0300

    Teste de commit

--- src/condicionais/Exercicio3.java
+++ src/condicionais/Exercicio3.java
@@ -3,6 +3,7 @@ package condicionais;
 public class Exercicio3 {
     public static void main(String[] args) {
         System.out.println("Teste de escrita no Java !");
+
         System.out.println("Teste de escrita no Java !");
     }
 }
