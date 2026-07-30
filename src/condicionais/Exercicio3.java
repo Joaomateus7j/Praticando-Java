e5e6da35cf09df0372195122932be7dd6e8e873b
João Mateus <mjoaomateus10@gmail.com>
2026-07-30 20:45:40 -0300

    Teste

--- src/condicionais/Exercicio3.java
+++ src/condicionais/Exercicio3.java
@@ -4,6 +4,7 @@ public class Exercicio3 {
     public static void main(String[] args) {
         System.out.println("Teste de escrita no Java !");
 
+
         System.out.println("Teste de escrita no Java !");
     }
 }
