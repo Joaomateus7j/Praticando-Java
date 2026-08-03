06925e8a12804c1f99b1a66e08378318b1c450c7
João Mateus <mjoaomateus10@gmail.com>
2026-08-03 15:21:53 -0300

    Teste

--- src/condicionais/Exercicio4.java
+++ src/condicionais/Exercicio4.java
@@ -3,5 +3,7 @@ package condicionais;
 public class Exercicio4 {
     public static void main(String[] args) {
     System.out.println("Teste de commitsss");
+
+    System.out.println("Teste de commitasss");
     }
 }
