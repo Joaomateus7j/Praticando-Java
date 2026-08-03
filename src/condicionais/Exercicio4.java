6002157d12942325b5db20c50c4ad6a1d4af3d43
João Mateus <mjoaomateus10@gmail.com>
2026-08-02 21:25:03 -0300

    Adicionando

--- src/condicionais/Exercicio4.java
+++ src/condicionais/Exercicio4.java
@@ -2,6 +2,6 @@ package condicionais;
 
 public class Exercicio4 {
     public static void main(String[] args) {
-    System.out.println("Teste de commit");
+    System.out.println("Teste de commitsss");
     }
 }
