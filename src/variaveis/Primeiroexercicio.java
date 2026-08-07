3c1a0d1046509eba2db1fbfbd05d2bc1457f0e60
João Mateus <mjoaomateus10@gmail.com>
2026-08-07 17:43:34 -0300

    Teste

--- src/variaveis/Primeiroexercicio.java
+++ src/variaveis/Primeiroexercicio.java
@@ -3,7 +3,7 @@ package variaveis;
 public class Primeiroexercicio {
     public static void main(String[] args) {
 
-        String teste2 = "Estou testando e criando minha pasta de váriavel, testando";
+        String teste2 = "Estou testando e criando minha pasta de váriavel, testando, Teste ";
         System.out.println(teste2);
     }
 }
