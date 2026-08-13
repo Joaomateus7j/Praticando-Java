be0f22e8f0c138ce0cd00727f1a6d0833088b939
João Mateus <mjoaomateus10@gmail.com>
2026-08-13 18:31:54 -0300

    Aplicando

--- src/condicionais/Exercicio2.java
+++ src/condicionais/Exercicio2.java
@@ -3,9 +3,5 @@ package condicionais;
 public class Exercicio2 {
     public static void main(String[] args) {
 
-        if(true){
-            System.out.println("Dentro do if");
-        }
-        System.out.println("Fora de if");
     }
 }
