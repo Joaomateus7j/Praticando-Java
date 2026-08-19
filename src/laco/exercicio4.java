37f106171418029afdf937af46685f5eac6c4939
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 18:17:29 -0300

    Aplicando while

---
+++ src/laco/exercicio4.java
@@ -0,0 +1,19 @@
+package laco;
+
+public class exercicio4 {
+    static void main(String[] args) {
+
+        // Desafio 4 : Somar os numeros de 1 a 10
+
+        int i = 1;
+        int soma = 0;
+
+        while (i <= 10) {
+            soma += i;
+            i++;
+
+        }
+        System.out.println(soma);
+
+    }
+}
---
+++ src/laco/exercicio5.java
@@ -0,0 +1,20 @@
+package laco;
+
+public class exercicio5 {
+    static void main(String[] args) {
+
+        // Desafio 5: Contagem regressiva de números ímpares
+
+        int numero = 15;
+        while (numero >= 1) {
+            System.out.println(numero);
+            numero -= 2;
+
+        }
+
+        
+
+
+
+    }
+}
