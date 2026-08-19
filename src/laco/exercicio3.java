4817965b3f440d509f04089f9266cd3474078bf9
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 17:35:02 -0300

    Praticando while

--- src/laco/exercicio3.java
+++ src/laco/exercicio3.java
@@ -3,6 +3,13 @@ package laco;
 public class exercicio3 {
     static void main(String[] args) {
 
-        
+        int multiplicador = 1;
+        while (multiplicador <= 10) {
+            int resultado = 5* multiplicador;
+            System.out.println("5 x " + multiplicador + " = " + resultado);
+
+            multiplicador++;
+
+        }
     }
 }
