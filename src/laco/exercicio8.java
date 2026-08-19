51c51f04e96720fd550c8fae2610c528c80115bc
João Mateus <mjoaomateus10@gmail.com>
2026-08-19 18:41:26 -0300

    Aplicando while

---
+++ src/laco/exercicio8.java
@@ -0,0 +1,17 @@
+package laco;
+
+public class exercicio8 {
+    static void main(String[] args) {
+
+        double  valorRestante = 130;
+        int nota = 20;
+
+        while (valorRestante >= 20) {
+            valorRestante-= 20;
+            
+
+        }
+        System.out.println(valorRestante);
+
+    }
+}
