b3feed8f2200cc95be0b928ca125c34d0143cb42
João Mateus <mjoaomateus10@gmail.com>
2026-09-14 19:36:01 -0300

    Aplicando

--- src/condicionais/Exercicio1.java
+++ src/condicionais/Exercicio1.java
@@ -3,18 +3,10 @@ package condicionais2;
 public class Exercicio1 {
     public static void main(String[] args) {
 
-        // Desafio do aluno
+        String nome = "Testando ";
 
-        double nota = 5;
-        String aluno;
-
-        if (nota >= 7) {
-            aluno = ("Aluno aprovado");
-        } else {
-            aluno = ("Aluno reprovado");
-        }
-
-        System.out.println(aluno);
+        System.out.println(nome);
+        
 
 
     }
